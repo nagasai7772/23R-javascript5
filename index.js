fa7cf77c3ae6a5fee011b2c906e660a358242a9d
nagasai7772 <nagunagasai@gmail.com>
2024-10-23 09:03:36 +0530

    For-in for-of object methods

---
+++ index.js
@@ -0,0 +1,33 @@
+// var car={brand:'Toyota',model:'corolla',year:'2020'};
+// for(names in car)
+// {
+//     console.log(`${names} : ${car[names].toUpperCase()}`);
+
+// }
+// var numbers=[1,2,3,4,5];
+// for(position in numbers)
+// {
+//     console.log(`${numbers[position]}-HI`);
+    
+// }
+// var fruits=['Apple','Banana','Cherry','Date'];
+// for(line in fruits)
+// {
+//     console.log(`${line}-${fruits[line].toUpperCase()}`)
+// }
+
+// var obj={
+//     name:'John',age:'30',address:{
+//         city:'Los Angeles',state:'CA'
+//     }
+// }
+// obj.address.city="SanFrancisco";
+// console.log(obj);
+
+// var car={brand:'Toyota',model:'corolla',year:'2020'};
+// car.year="2022";
+// console.log(car);
+
+// var receipe={name:'pasta',servings:'2',ingredients:['noodels','sauce']};
+// receipe.ingredients[2]='Cheese';
+// console.log(receipe);
